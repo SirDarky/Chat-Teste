527222a667c3e4f0724342bc6478930a72b11a20
Marcus Vinicius <marcus.profissional12@gmail.com>
2022-12-14 14:06:33 -0400

    vercel update

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-'use strict';
-
-var ip = require("ip");
-console.dir ( ip.address() );
-
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
-const Sala = require('./newSala');
+const Sala = require('./src/newSala');
 const app = express()
 const server = http.createServer(app);
 const io = new socketio.Server(server, {
